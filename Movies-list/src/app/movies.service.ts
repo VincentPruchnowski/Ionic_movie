b60ee870e475212051449b49c38b3aa9d6d6d27f
Olcou <olivier.coulon43@gmail.com>
2022-02-09 10:58:42 +0100

    liste de films avec genre
    liste film v2

--- Movies-list/src/app/movies.service.ts
+++ Movies-list/src/app/movies.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 export interface Movie {
   id: string;
   title: string;
+  genre: string;
   releaseDate?: string;
   directorName?: string;
   directorName2?: string;
@@ -16,18 +17,18 @@ export interface Movie {
 export class MoviesService {
   private currentMovie: Movie;
   Movies: Movie[] = [
-    { id: '1', title: 'Gremlins', releaseDate: '1984', directorName: 'Joe Dante', photoUrl:'https://image.posterlounge.fr/images/l/1896339.jpg' },
-    { id: '2', title: 'Le Septième Sceau', releaseDate: '1957', directorName: 'Ingmar Bergman', photoUrl:'https://p7.storage.canalblog.com/75/00/1349409/128073201_o.jpg' },
-    { id: '3', title: 'Inferno', releaseDate: '1980', directorName: 'Dario Argento', photoUrl:'https://media.senscritique.com/media/000016132349/source_big/Inferno.jpg' },
-    { id: '4', title: 'Le Jour des morts-vivants', releaseDate: '1985', directorName: 'George A. Romero', photoUrl:'https://media.senscritique.com/media/000019420985/source_big/Le_Jour_des_morts_vivants.jpg' },
-    { id: '5', title: 'Bagarres', releaseDate: '1948', directorName: 'Henri Calef', photoUrl:'https://media.senscritique.com/media/000014010145/source_big/Bagarres.jpg' },
-    { id: '6', title: 'Midsommar ', releaseDate: '2019', directorName: 'Ari Aster', photoUrl:'https://media.senscritique.com/media/000018609294/source_big/Midsommar.jpg' },
-    { id: '7', title: 'The Devil\'s Rejects', releaseDate: '2005', directorName: 'Rob Zombie', photoUrl:'https://media.senscritique.com/media/000020090905/source_big/The_Devil_s_Rejects.jpg' },
-    { id: '8', title: 'Porco Rosso', releaseDate: '1992', directorName: 'Hayao Miyazaki', photoUrl:'https://media.senscritique.com/media/000007365703/source_big/Porco_Rosso.png' },
-    { id: '9', title: 'Fascination', releaseDate: '1979', directorName: 'Jean Rollin', photoUrl:'https://media.senscritique.com/media/000018508577/source_big/Fascination.jpg' },
-    { id: '10', title: 'Conan le Barbare', releaseDate: '1982', directorName: 'John Milius', photoUrl:'https://media.senscritique.com/media/000015224775/source_big/Conan_le_Barbare.jpg' },
-    { id: '11', title: 'Climax', releaseDate: '2018', directorName: 'Gaspar Noé ', photoUrl:'https://media.senscritique.com/media/000017960616/source_big/Climax.jpg' },
-    { id: '12', title: 'Dark Crystal', releaseDate: '1982', directorName: ' Jim Henson ', directorName2: 'Frank Oz ', photoUrl:'https://media.senscritique.com/media/000017258442/source_big/Dark_Crystal.jpg' }
+    { id: '1', title: 'Gremlins', genre:'comédie / horreur', releaseDate: '1984', directorName: 'Joe Dante', photoUrl:'https://image.posterlounge.fr/images/l/1896339.jpg' },
+    { id: '2', title: 'Le Septième Sceau',genre:'drame / fantastique', releaseDate: '1957', directorName: 'Ingmar Bergman', photoUrl:'https://p7.storage.canalblog.com/75/00/1349409/128073201_o.jpg' },
+    { id: '3', title: 'Inferno', genre:'fantastique / horreur',releaseDate: '1980', directorName: 'Dario Argento', photoUrl:'https://media.senscritique.com/media/000016132349/source_big/Inferno.jpg' },
+    { id: '4', title: 'Le Jour des morts-vivants', genre:'horreur', releaseDate: '1985', directorName: 'George A. Romero', photoUrl:'https://media.senscritique.com/media/000019420985/source_big/Le_Jour_des_morts_vivants.jpg' },
+    { id: '5', title: 'Bagarres', genre:'drame', releaseDate: '1948', directorName: 'Henri Calef', photoUrl:'https://media.senscritique.com/media/000014010145/source_big/Bagarres.jpg' },
+    { id: '6', title: 'Midsommar ', genre:'fantastique', releaseDate: '2019', directorName: 'Ari Aster', photoUrl:'https://media.senscritique.com/media/000018609294/source_big/Midsommar.jpg' },
+    { id: '7', title: 'The Devil\'s Rejects', genre:'horreur', releaseDate: '2005', directorName: 'Rob Zombie', photoUrl:'https://media.senscritique.com/media/000020090905/source_big/The_Devil_s_Rejects.jpg' },
+    { id: '8', title: 'Porco Rosso', genre:'manga', releaseDate: '1992', directorName: 'Hayao Miyazaki', photoUrl:'https://media.senscritique.com/media/000007365703/source_big/Porco_Rosso.png' },
+    { id: '9', title: 'Fascination', genre:'fantastique / épouvante', releaseDate: '1979', directorName: 'Jean Rollin', photoUrl:'https://media.senscritique.com/media/000018508577/source_big/Fascination.jpg' },
+    { id: '10', title: 'Conan le Barbare',genre:'heroic-fantasy', releaseDate: '1982', directorName: 'John Milius', photoUrl:'https://media.senscritique.com/media/000015224775/source_big/Conan_le_Barbare.jpg' },
+    { id: '11', title: 'Climax', genre:'drame / horreur',releaseDate: '2018', directorName: 'Gaspar Noé ', photoUrl:'https://media.senscritique.com/media/000017960616/source_big/Climax.jpg' },
+    { id: '12', title: 'Dark Crystal', genre:'heroic-fantasy', releaseDate: '1982', directorName: ' Jim Henson ', directorName2: 'Frank Oz ', photoUrl:'https://media.senscritique.com/media/000017258442/source_big/Dark_Crystal.jpg' }
     
   ];
 
